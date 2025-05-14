618767f17757b867e3bd653408292fb216a655ad
saoski <saoski88@gmail.com>
2025-05-14 12:10:45 -0400

    Fix package path for autodoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ release = '0.1.1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 import sys
 from pathlib import Path
-sys.path.insert(0, str(Path('..', '..').resolve()))
+sys.path.insert(0, str(Path('..', '..', 'src').resolve()))
 
 extensions = ['sphinx.ext.autodoc',]
 
